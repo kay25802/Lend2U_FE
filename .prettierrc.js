cced50098c22800e990f58ff900663b59aecceae
Kim A Young <119282789+kay25802@users.noreply.github.com>
2024-06-04 21:32:38 +0900

    Add feature: Application code

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
+  trailingComma: 'none'
+};
